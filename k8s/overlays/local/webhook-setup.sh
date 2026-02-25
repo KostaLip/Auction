fb8a32abec5c457f494df368aecfd07ccd78c631
KostaLip <sprdils.gen@tgmail.com>
2026-02-25 16:50:23 +0100

    Added priority class, kustomization enviroments, horizontal pod autoscalers and started network policies

---
+++ k8s/overlays/local/webhook-setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+kubectl apply -f ../../base/mutating-webhook-namespace.yaml
+kubectl apply -f ../../base/secrets/webhook-tls-secret.yaml
+kubectl apply -f ../../base/admission-controller-service.yaml
+kubectl apply -f ../../base/admission-controller-deployment.yaml
+kubectl apply -f ../../base/mutating-webhook-configuration.yaml
